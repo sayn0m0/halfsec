3b16083983d82892ae1255a5762b5d5fbe2a6cd7
Ian <steven.ford.j@gmail.com>
2022-10-05 23:37:50 +0100

    typeorm jwt

--- apps/nest/src/app.module.ts
+++ apps/nest/src/app.module.ts
@@ -1,8 +1,23 @@
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './config';
+
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: config.DB.HOSTNAME,
+      port: config.DB.PORT,
+      username: config.DB.USERNAME,
+      password: config.DB.PASSWORD,
+      database: config.DB.DB_NAME,
+      entities: [],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- apps/nest/src/config.ts
+++ apps/nest/src/config.ts
@@ -6,12 +6,26 @@ interface IEnv {
   FE_URL: string;
   DEVELOPMENT: string;
   PORT: string;
+  DB: {
+    HOSTNAME: string;
+    PORT: number;
+    USERNAME: string;
+    PASSWORD: string;
+    DB_NAME: string;
+  };
 }
 
 const env: IEnv = {
   FE_URL: process.env.FE_URL!,
   DEVELOPMENT: process.env.DEVELOPMENT!,
   PORT: process.env.PORT,
+  DB: {
+    HOSTNAME: process.env.PG_HOSTNAME!,
+    PORT: Number(process.env.PG_PORT!),
+    USERNAME: process.env.PG_USERNAME!,
+    PASSWORD: process.env.PG_PASSWORD!,
+    DB_NAME: process.env.PG_DB_NAME!,
+  },
 };
 
 export default env;
